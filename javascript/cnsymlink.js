4bdb2c6453e34777014780004aec152b2d5f842a
prolapse <117679376+PR0LAPSE@users.noreply.github.com>
2023-12-29 10:50:39 +0500

    Update cnsymlink.js

--- javascript/cnsymlink.js
+++ javascript/cnsymlink.js
@@ -1,18 +1,34 @@
-function moveSymlinkerButtons(scriptContainerId) {
-    var scriptContainer = document.getElementById(scriptContainerId);
-    var cnSymlinkButtons = document.createElement("div");
-    cnSymlinkButtons.id = "cn_symlinker_buttons";
-    var sdCnButton = scriptContainer.querySelector("#cn_sd_symlinker_button");
-    var sdxlCnButton = scriptContainer.querySelector("#cn_sdxl_symlinker_button");
-    var controlnetExtTabs = scriptContainer.querySelector("#" + scriptContainerId + "_controlnet_tabs");
+function moveSymlinkerButtons() {
+    setTimeout(function () {
+        var txt2img_script_container = document.getElementById("txt2img_script_container");
+        var cn_symlink_buttons_txt2img = document.createElement("div");
+        cn_symlink_buttons_txt2img.id = "cn_symlinker_buttons";
+        var sd_cn_button_txt2img = txt2img_script_container.querySelector("#cn_sd_symlinker_button");
+        var sdxl_cn_button_txt2img = txt2img_script_container.querySelector("#cn_sdxl_symlinker_button");
+        var controlnet_ext_tabs_txt2img = txt2img_script_container.querySelector("#txt2img_controlnet_tabs");
+        sd_cn_button_txt2img.parentNode.insertBefore(cn_symlink_buttons_txt2img, sd_cn_button_txt2img);
+        cn_symlink_buttons_txt2img.appendChild(sd_cn_button_txt2img);
+        cn_symlink_buttons_txt2img.appendChild(sdxl_cn_button_txt2img);
+        var cn_symlink_container_txt2img = cn_symlink_buttons_txt2img.parentNode;
+        cn_symlink_container_txt2img.id = "cn_symlynk_container";
+        controlnet_ext_tabs_txt2img.insertBefore(cn_symlink_container_txt2img, controlnet_ext_tabs_txt2img.firstChild);
     
-        sdCnButton.parentNode.insertBefore(cnSymlinkButtons, sdCnButton);
-        cnSymlinkButtons.appendChild(sdCnButton);
-        cnSymlinkButtons.appendChild(sdxlCnButton);
-        var cnSymlinkContainer = cnSymlinkButtons.parentNode;
-        cnSymlinkContainer.id = "cn_symlynk_container";
-        controlnetExtTabs.insertBefore(cnSymlinkContainer, controlnetExtTabs.firstChild);
+    }, 1000);
     
+    setTimeout(function () {
+        var img2img_script_container = document.getElementById("img2img_script_container");
+        var cn_symlink_buttons_img2img = document.createElement("div");
+        cn_symlink_buttons_img2img.id = "cn_symlinker_buttons";
+        var sd_cn_button_img2img = img2img_script_container.querySelector("#cn_sd_symlinker_button");
+        var sdxl_cn_button_img2img = img2img_script_container.querySelector("#cn_sdxl_symlinker_button");
+        var controlnet_ext_tabs_img2img = img2img_script_container.querySelector("#img2img_controlnet_tabs");
+        sd_cn_button_img2img.parentNode.insertBefore(cn_symlink_buttons_img2img, sd_cn_button_img2img);
+        cn_symlink_buttons_img2img.appendChild(sd_cn_button_img2img);
+        cn_symlink_buttons_img2img.appendChild(sdxl_cn_button_img2img);
+        var cn_symlink_container_img2img = cn_symlink_buttons_img2img.parentNode;
+        cn_symlink_container_img2img.id = "cn_symlynk_container";
+        controlnet_ext_tabs_img2img.insertBefore(cn_symlink_container_img2img, controlnet_ext_tabs_img2img.firstChild);
+    }, 1500);
 }
 
 
@@ -68,10 +84,8 @@ const elementsToObserve = [
 
 document.addEventListener('DOMContentLoaded', function () {
     onUiLoaded(function () {
-        
         onElementsLoaded(elementsToObserve, () => {
-            moveSymlinkerButtons("txt2img_script_container");
-            moveSymlinkerButtons("img2img_script_container");
+            moveSymlinkerButtons();
         });
     });
 });
