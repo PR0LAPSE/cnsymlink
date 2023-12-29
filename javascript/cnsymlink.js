63ec34eab7b6117142986eb6fe1391ef61c272d9
prolapse <117679376+PR0LAPSE@users.noreply.github.com>
2023-12-29 10:24:38 +0500

    Update cnsymlink.js

--- javascript/cnsymlink.js
+++ javascript/cnsymlink.js
@@ -1,24 +1,18 @@
 function moveSymlinkerButtons(scriptContainerId) {
     var scriptContainer = document.getElementById(scriptContainerId);
-    if (scriptContainer) {
-        var cnSymlinkButtons = document.createElement("div");
-        cnSymlinkButtons.id = "cn_symlinker_buttons";
-        var sdCnButton = scriptContainer.querySelector("#cn_sd_symlinker_button");
-        var sdxlCnButton = scriptContainer.querySelector("#cn_sdxl_symlinker_button");
-        var controlnetExtTabs = scriptContainer.querySelector("#" + scriptContainerId + "_controlnet_tabs");
-        if (sdCnButton && sdxlCnButton && controlnetExtTabs) {
-            sdCnButton.parentNode.insertBefore(cnSymlinkButtons, sdCnButton);
-            cnSymlinkButtons.appendChild(sdCnButton);
-            cnSymlinkButtons.appendChild(sdxlCnButton);
-            var cnSymlinkContainer = cnSymlinkButtons.parentNode;
-            cnSymlinkContainer.id = "cn_symlynk_container";
-            controlnetExtTabs.insertBefore(cnSymlinkContainer, controlnetExtTabs.firstChild);
-        } else {
-            console.log("дополнения не загружены?");
-        }
-    } else {
-        console.log("контейнер скриптов не найден");
-    }
+    var cnSymlinkButtons = document.createElement("div");
+    cnSymlinkButtons.id = "cn_symlinker_buttons";
+    var sdCnButton = scriptContainer.querySelector("#cn_sd_symlinker_button");
+    var sdxlCnButton = scriptContainer.querySelector("#cn_sdxl_symlinker_button");
+    var controlnetExtTabs = scriptContainer.querySelector("#" + scriptContainerId + "_controlnet_tabs");
+    
+        sdCnButton.parentNode.insertBefore(cnSymlinkButtons, sdCnButton);
+        cnSymlinkButtons.appendChild(sdCnButton);
+        cnSymlinkButtons.appendChild(sdxlCnButton);
+        var cnSymlinkContainer = cnSymlinkButtons.parentNode;
+        cnSymlinkContainer.id = "cn_symlynk_container";
+        controlnetExtTabs.insertBefore(cnSymlinkContainer, controlnetExtTabs.firstChild);
+    
 }
 
 
